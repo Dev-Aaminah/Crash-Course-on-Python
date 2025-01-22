6cf899fb8e69aec822b8044f3546adf81c57d290
Dev-Aaminah <aminakanwal12345@gmail.com>
2025-01-22 20:48:53 +0500

    dunder methods in pythons

--- 40_special.py
+++ 40_special.py
@@ -61,6 +61,25 @@ class Employee:
     
     def __str__(self):
         return '{} - {}'.format(self.fullname(), self.email())
+    
+    def __add__(self, other):
+        return self.pay + other.pay
 emp1 = Employee('Corey', 'Schafer', 50000)
 print(emp1)
 
+emp2 = Employee('Steven', 'Schafer', 60000)
+print(emp2)
+
+print(emp1.__repr__())
+print(emp1.__str__())
+# Special method for arithematic
+print(1+2)
+print('a'+'b')
+
+print(int.__add__(1,2))
+print(str.__add__('a','b'))
+
+print(emp1+emp2)
+
+print(len('Aaminah'))
+print('Aaminah'.__len__())
